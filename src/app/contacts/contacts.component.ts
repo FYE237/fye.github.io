d4851f4342507434f4e8dc749544c20cc1a35758
FEZEU YOUMBI Emmanuel <yfezeu.emma@gmail.com>
2023-02-17 01:26:37 +0100

    TOPIC : Contacts OK

--- src/app/contacts/contacts.component.ts
+++ src/app/contacts/contacts.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { GestionAffichageService } from '../service/gestion-affichage.service';
 
 @Component({
   selector: 'app-contacts',
@@ -7,9 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ContactsComponent implements OnInit {
 
-  constructor() { }
+  constructor(private myService : GestionAffichageService) {      this.myService.showfooter();
+  }
 
   ngOnInit(): void {
   }
+
+
 
 }
